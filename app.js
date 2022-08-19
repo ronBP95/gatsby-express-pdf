9838a23d297f433a7c01cd598dba7d714270149e
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-18 23:29:29 -0700

    Made the main root route the resume access since I am listing this as a subdomain

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000
 var file = fs.createReadStream('./public/pdfs/resume.pdf');
 var stat = fs.statSync('./public/pdfs/resume.pdf');
 
-app.get('/resume', function (req, res) {
+app.get('/', function (req, res) {
         res.setHeader('Content-Length', stat.size);
         res.setHeader('Content-Type', 'application/pdf');
         res.setHeader('Content-Disposition', 'inline; filename=resume.pdf');
