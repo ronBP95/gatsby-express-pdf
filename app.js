d21fd6cbda58e163bf155384cfc7c968e10a7551
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-18 22:58:48 -0700

    Added resume only path

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ const port = process.env.PORT || 5000
 var file = fs.createReadStream('./public/pdfs/resume.pdf');
 var stat = fs.statSync('./public/pdfs/resume.pdf');
 
-app.get('/', function name(req, res) {
-        res.send('Hello World')
-})
-
 app.get('/resume', function (req, res) {
         res.setHeader('Content-Length', stat.size);
         res.setHeader('Content-Type', 'application/pdf');
