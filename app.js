a908753cb5ed0eb27a2c5ddb17d94b66fa22067a
Ron Magpantay <76860289+ronBP95@users.noreply.github.com>
2022-08-18 21:52:04 -0700

    Stored the pdf file in Express server

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const port = 3000;
+let fs = require('fs')
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.set("view engine", "ejs");
+// variables
+var file = fs.createReadStream('./public/pdfs/resume.pdf');
+var stat = fs.statSync('./public/pdfs/resume.pdf');
+
+app.get('/', function (req, res) {
+        res.setHeader('Content-Length', stat.size);
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'inline; filename=resume.pdf');
+        file.pipe(res);
+})
+
+// app.set("view engine", "ejs");
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
